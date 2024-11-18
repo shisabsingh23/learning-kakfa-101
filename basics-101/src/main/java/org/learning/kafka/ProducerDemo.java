d21a8641ff6c251bbe3ca2b0437582128d164cd8
shisab <shisab@shisabs-mbp.myfiosgateway.com>
2024-11-18 16:25:10 -0500

    added module and removed main src fldr

---
+++ basics-101/src/main/java/org/learning/kafka/ProducerDemo.java
@@ -0,0 +1,7 @@
+package org.learning.kafka;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        
+    }
+}
--- basics-101/src/test/java/org/learning/kafka/AppTest.java
+++ basics-101/src/test/java/org/learning/kafka/AppTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.learning.kafka;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
